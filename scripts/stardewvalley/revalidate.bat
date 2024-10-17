3b2e556b0a12c440aca1cba0ed4c60d3ab349ad8
Tien Duc Le <6162574+tienducle@users.noreply.github.com>
2024-10-17 20:36:03 +0200

    Fixed windows batch scripts to handle paths with spaces correctly

--- scripts/stardewvalley/revalidate.bat
+++ scripts/stardewvalley/revalidate.bat
@@ -1,13 +1,13 @@
 @echo off
-set ORIGINAL_FILE_PATH=%1
-set TARGET_LANGUAGE=%2
+set ORIGINAL_FILE_PATH=%~1
+set TARGET_LANGUAGE=%~2
 
-if %ORIGINAL_FILE_PATH% == "" (
+if "%ORIGINAL_FILE_PATH%" == "" (
     echo "Missing ORIGINAL_FILE_PATH (arg1)"
     exit /b 1
 )
 
-if %TARGET_LANGUAGE% == "" (
+if "%TARGET_LANGUAGE%" == "" (
     echo "Missing TARGET_LANGUAGE (arg2, e.g. German, French, Spanish, etc.)"
     exit /b 1
 )
